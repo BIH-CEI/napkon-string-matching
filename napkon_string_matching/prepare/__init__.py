79f0e85d48df83acde88d8e12ebdcb32d15d58c5
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-20 14:13:51 +0200

    rename module containing token generation

--- napkon_string_matching/prepare/__init__.py
+++ napkon_string_matching/prepare/__init__.py
@@ -1,2 +1,2 @@
+from napkon_string_matching.prepare.generate import gen_token
 from napkon_string_matching.prepare.match_preparator import MatchPreparator
-from napkon_string_matching.prepare.token import gen_token
