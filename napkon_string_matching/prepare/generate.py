501ab66eb497ca5343099589515640a35b8a3069
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-28 08:57:20 +0200

    forward term as token list and remove unwanted symbols

--- napkon_string_matching/prepare/generate.py
+++ napkon_string_matching/prepare/generate.py
@@ -5,6 +5,7 @@ import nltk
 import numpy as np
 import pandas as pd
 from napkon_string_matching.prepare import PREPARE_COLUMN_SCORE
+from napkon_string_matching.prepare.constants import PREPARE_REMOVE_SYMBOLS
 from napkon_string_matching.terminology import (
     TERMINOLOGY_COLUMN_ID,
     TERMINOLOGY_COLUMN_TERM,
@@ -19,7 +20,10 @@ from nltk.tokenize import word_tokenize
 
 
 def gen_tokens(
-    term: str, reference: pd.DataFrame, headings: pd.DataFrame, score_threshold: int
+    term: List[str],
+    reference: pd.DataFrame,
+    headings: pd.DataFrame,
+    score_threshold: int,
 ) -> Tuple[List[str], List[str], Dict[str, str]]:
     """
     Generate tokens from term, references and headings
@@ -31,8 +35,9 @@ def gen_tokens(
     ref_copy = reference.copy(deep=True)
 
     # Calculate the score for each combination
-    ref_copy[PREPARE_COLUMN_SCORE] = np.vectorize(fuzz.partial_token_set_ratio)(
-        ref_copy[TERMINOLOGY_COLUMN_TERM], term
+    term_str = " ".join(term)
+    ref_copy[PREPARE_COLUMN_SCORE] = np.vectorize(fuzz.WRatio)(
+        ref_copy[TERMINOLOGY_COLUMN_TERM], term_str
     )
 
     # Get IDs above threshold
@@ -67,8 +72,10 @@ def gen_term(
     tokens = word_tokenize(" ".join(term_parts))
 
     stop_words = set(stopwords.words(language))
-    tokens = [word for word in tokens if word.casefold() not in stop_words]
+    tokens = {
+        word
+        for word in tokens
+        if word.casefold() not in stop_words and word not in PREPARE_REMOVE_SYMBOLS
+    }
 
-    tokens.sort()
-
-    return " ".join(tokens)
+    return sorted(tokens, key=str.casefold)
