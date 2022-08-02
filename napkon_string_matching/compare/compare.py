f63d918f9e7e5aee0147aa36736e62599bc8e44a
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-08-02 08:17:38 +0200

    restructure to improve readability

--- napkon_string_matching/compare/compare.py
+++ napkon_string_matching/compare/compare.py
@@ -14,51 +14,75 @@ from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
+SUFFIX_LEFT = "_left"
+SUFFIX_RIGHT = "_right"
+
+COMPARE_COLUMN = DATA_COLUMN_TOKEN_IDS
+
 
 def _hash_dataframes(*dfs) -> str:
-    return "_".join([md5(df.to_csv().encode("utf-8")).hexdigest() for df in dfs])
+    return "_".join(
+        [
+            md5(df.to_csv().encode("utf-8"), usedforsecurity=False).hexdigest()
+            for df in dfs
+        ]
+    )
 
 
 def compare(dataset_left: pd.DataFrame, dataset_right: pd.DataFrame):
-    SUFFIX_LEFT = "_left"
-    SUFFIX_RIGHT = "_right"
-
-    COMPARE_COLUMN = DATA_COLUMN_TOKEN_IDS
 
     df_hash = _hash_dataframes(dataset_left, dataset_right)
-    cache_score_file = Path("compare") / ("cache_score_" + df_hash + ".json")
-    if cache_score_file.exists():
-        logger.info("using cached score")
-        logger.debug("reading from %s", cache_score_file)
-        compare_df = dataframe.read(cache_score_file)
-        logger.debug("got %i entries", len(compare_df))
-    else:
-        df1_filtered = _get_na_filtered(dataset_left, column=COMPARE_COLUMN)
-        df2_filtered = _get_na_filtered(dataset_right, column=COMPARE_COLUMN)
+    cache_score_file = Path("compared") / ("cache_score_" + df_hash + ".json")
 
-        compare_df = _gen_permutation(
-            df1_filtered,
-            df2_filtered,
-            columns=[DATA_COLUMN_IDENTIFIER, COMPARE_COLUMN],
-            suffix_left=SUFFIX_LEFT,
-            suffix_right=SUFFIX_RIGHT,
+    # Get the compare dataframe that holds the score to match all entries from
+    # the left with each from right dataset
+    if cache_score_file.exists():
+        compare_df = _read_compare_dataframe(cache_score_file)
+    else:
+        compare_df = _gen_compare_dataframe(
+            dataset_left, dataset_right, cache_score_file
         )
 
-        logger.info("calculate score")
-        compare_df["Score"] = [
-            _calc_score(row, SUFFIX_LEFT, SUFFIX_RIGHT)
-            for _, row in tqdm(compare_df.iterrows(), total=len(compare_df))
-        ]
 
-        compare_df = compare_df[compare_df["Score"] > 0]
-        logger.debug("got %i entries", len(compare_df))
+def _read_compare_dataframe(cache_file: Path) -> pd.DataFrame:
+    logger.info("using cached score")
+    logger.debug("reading from %s", cache_file)
+    compare_df = dataframe.read(cache_file)
+    logger.debug("got %i entries", len(compare_df))
+    return compare_df
 
-        if not cache_score_file.parent.exists():
-            cache_score_file.parent.mkdir(parents=True)
 
-        logger.info("write cache to file")
-        logger.debug("write to %s", cache_score_file)
-        dataframe.write(cache_score_file, compare_df)
+def _gen_compare_dataframe(
+    df_left: pd.DataFrame, df_right: pd.DataFrame, cache_file: Path
+) -> pd.DataFrame:
+    df1_filtered = _get_na_filtered(df_left, column=COMPARE_COLUMN)
+    df2_filtered = _get_na_filtered(df_right, column=COMPARE_COLUMN)
+
+    compare_df = _gen_permutation(
+        df1_filtered,
+        df2_filtered,
+        columns=[DATA_COLUMN_IDENTIFIER, COMPARE_COLUMN],
+        suffix_left=SUFFIX_LEFT,
+        suffix_right=SUFFIX_RIGHT,
+    )
+
+    logger.info("calculate score")
+    compare_df["Score"] = [
+        _calc_score(row, SUFFIX_LEFT, SUFFIX_RIGHT)
+        for _, row in tqdm(compare_df.iterrows(), total=len(compare_df))
+    ]
+
+    compare_df = compare_df[compare_df["Score"] > 0]
+    logger.debug("got %i entries", len(compare_df))
+
+    if not cache_file.parent.exists():
+        cache_file.parent.mkdir(parents=True)
+
+    logger.info("write cache to file")
+    logger.debug("write to %s", cache_file)
+    dataframe.write(cache_file, compare_df)
+
+    return compare_df
 
 
 def _get_na_filtered(df: pd.DataFrame, column: str) -> pd.DataFrame:
