3ef5aff62a7cb004495db10a218fd90bb9caefbc
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-08-15 12:39:52 +0200

    made score function more generalised to accept either a string or a tokenized string

--- napkon_string_matching/compare/score_functions.py
+++ napkon_string_matching/compare/score_functions.py
@@ -3,15 +3,19 @@ from typing import List
 from rapidfuzz import fuzz
 
 
-def intersection_vs_union(left: List[str], right: List[str]) -> float:
-    set_left = set(left)
-    set_right = set(right)
+def intersection_vs_union(left: List[str] | str, right: List[str] | str) -> float:
+    set_left = set(left if isinstance(left, list) else left.split())
+    set_right = set(right if isinstance(right, list) else right.split())
 
     return len(set_left.intersection(set_right)) / len(set_left.union(set_right))
 
 
-def fuzzy_match(left: List[str], right: List[str]) -> float:
-    left_term = " ".join(sorted(left, key=str.lower))
-    right_term = " ".join(sorted(right, key=str.lower))
+def join_sorted(value: List[str]) -> str:
+    return " ".join(sorted(value, key=str.lower))
+
+
+def fuzzy_match(left: str | List[str], right: str | List[str]) -> float:
+    left_term = join_sorted(left) if isinstance(left, list) else left
+    right_term = join_sorted(right) if isinstance(right, list) else right
 
     return fuzz.QRatio(left_term, right_term) / 100
