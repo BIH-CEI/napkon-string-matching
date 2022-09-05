966e0d0949d328af3d94250883cbfa09a61ff468
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-05 14:52:52 +0200

    configure which steps and order using config

--- napkon_string_matching/matching.py
+++ napkon_string_matching/matching.py
@@ -9,6 +9,7 @@ from napkon_string_matching.prepare.match_preparator import MatchPreparator
 
 CONFIG_FIELD_PREPARE = "prepare"
 CONFIG_FIELD_MATCHING = "matching"
+CONFIG_FIELD_STEPS = "steps"
 
 
 logger = logging.getLogger(__name__)
@@ -18,9 +19,14 @@ def match(config: Dict) -> None:
     preparator = MatchPreparator(config[CONFIG_FIELD_PREPARE])
     matcher = Matcher(preparator, config)
 
-    matcher.match_questionnaires_variables()
-    matcher.match_gecco_with_questionnaires()
-    matcher.match_questionnaires()
+    for step in config[CONFIG_FIELD_STEPS]:
+        match step:
+            case "variables":
+                matcher.match_questionnaires_variables()
+            case "gecco":
+                matcher.match_gecco_with_questionnaires()
+            case "questionnaires":
+                matcher.match_questionnaires()
 
     matcher.print_analysis()
 
