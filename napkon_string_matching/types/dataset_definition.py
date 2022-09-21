a667e3bf5fae639fa755ccf1e05b55a17e0e1c98
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-21 13:50:17 +0200

    add sub-tables to dataset definitions

--- napkon_string_matching/types/dataset_definition.py
+++ napkon_string_matching/types/dataset_definition.py
@@ -14,23 +14,29 @@ COLUMN_VARIABLE = "Variable"
 
 DEFINITION_TABLE_ITEMS = "table_items"
 DEFINITION_TABLE_NAMES = "table_names"
+DEFINITION_SUBTABLES = "subtables"
 
 
 class DatasetDefinition:
     def __init__(self, data: Dict[str, Dict[str, List[str]]] = None):
         self._table_items: DefinitionTableItems = None
         self._table_names: DefinitionTableNames = None
+        self._subtables: DefinitionSubtables = None
 
         if data:
             if table_items := data.get(DEFINITION_TABLE_ITEMS):
                 self._table_items = DefinitionTableItems(table_items)
             if table_names := data.get(DEFINITION_TABLE_NAMES):
                 self._table_names = DefinitionTableNames(table_names)
+            if subtables := data.get(DEFINITION_SUBTABLES):
+                self._subtables = DefinitionSubtables(subtables)
 
         if not self._table_items:
             self._table_items = DefinitionTableItems()
         if not self._table_names:
             self._table_names = DefinitionTableNames()
+        if not self._subtables:
+            self._subtables = DefinitionSubtables()
 
     @property
     def table_items(self):
@@ -44,13 +50,15 @@ class DatasetDefinition:
         return {
             DEFINITION_TABLE_ITEMS: self._table_items.to_dict(),
             DEFINITION_TABLE_NAMES: self._table_names.to_dict(),
+            DEFINITION_SUBTABLES: self._subtables.to_dict(),
         }
 
     @classmethod
-    def read_csv(cls, column_file: str | Path, tables_file: str | Path):
+    def read_csv(cls, column_file: str | Path, tables_file: str | Path, dataset_file: str | Path):
         dataset = DatasetDefinition()
         dataset._table_items = DefinitionTableItems.read_csv(column_file)
         dataset._table_names = DefinitionTableNames.read_csv(tables_file)
+        dataset._subtables = DefinitionSubtables.read_csv(dataset_file)
         return dataset
 
 
@@ -75,8 +83,10 @@ class DatasetDefinitions:
         data = json.loads(Path(file).read_text())
         return cls(data)
 
-    def add_from_file(self, item: str, column_file: str | Path, tables_file: str | Path) -> None:
-        self[item] = DatasetDefinition.read_csv(column_file, tables_file)
+    def add_from_file(
+        self, item: str, column_file: str | Path, tables_file: str | Path, dataset_file: str | Path
+    ) -> None:
+        self[item] = DatasetDefinition.read_csv(column_file, tables_file, dataset_file)
 
 
 class DefinitionTableItems:
@@ -144,5 +154,46 @@ class DefinitionTableNames:
                 )
                 continue
             result[table.lower()] = name
+        logger.info("got %i tables", len(result.data.keys()))
+        return result
+
+
+class DefinitionSubtables:
+    def __init__(self, data: Dict[str, List[str]] = None):
+        self.data = data if data else {}
+
+    def __getitem__(self, item: str) -> List[str]:
+        return self.data.get(item, list())
+
+    def __setitem__(self, item: str, value: List[str]) -> None:
+        self.data[item] = value
+
+    def __contains__(self, item: str) -> bool:
+        return item in self.data
+
+    def to_dict(self) -> Dict[str, List[str]]:
+        return deepcopy(self.data)
+
+    @classmethod
+    def read_csv(cls, file: str | Path):
+        logger.info("read from file %s...", str(file))
+        df: pd.DataFrame = pd.read_csv(file, usecols=[3])
+        result = cls()
+        for table_string in pd.unique(df.iloc[:, 0]):
+            tables = table_string.split(", ")
+            if len(tables) <= 1:
+                continue
+            table = tables[0]
+            subtables = tables[1:]
+            table = table.lower()
+            if table in result:
+                logger.warning(
+                    "cannot assign subtables %s to table %s, already assigned %s",
+                    subtables,
+                    table,
+                    result[table],
+                )
+                continue
+            result[table] = subtables
         logger.info("got %i tables", len(result.data.keys()))
         return result
