a4163aa2eefa58807d3d9954873ef61bf2cd4c91
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-28 14:03:07 +0200

    added function to gather all subgroup names

---
+++ napkon_string_matching/misc.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from napkon_string_matching.types.questionnaire_types.dataset_table import DatasetTable
+
+
+def get_all_table_subgroup_name_combinations(dataset_tables: Dict[str, DatasetTable]):
+    """
+    Get a dictionary containing all subgroups and their groups a human readable name
+    for each DatasetTable.
+    """
+    result = {}
+    for name, dataset_table in dataset_tables.items():
+        result[name] = {}
+        for group, subgroups in dataset_table.subgroups.items():
+            result[name][dataset_table.groups[group]] = [
+                dataset_table.subgroup_names[subgroup] for subgroup in subgroups
+            ]
+    return result
