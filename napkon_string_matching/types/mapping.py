03acfd3962d754972f26e390cb493ecdc0414257
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-11-21 11:22:14 +0100

    new mapping format

--- napkon_string_matching/types/mapping.py
+++ napkon_string_matching/types/mapping.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from typing import Dict, List
+from uuid import uuid4
 
 from napkon_string_matching.types.base.readable_json import ReadableJson
 from napkon_string_matching.types.base.writable_json import WritableJson
@@ -160,3 +161,66 @@ class Mapping(ReadableJson, WritableJson):
 
     def __len__(self) -> int:
         return len(self.hap) + len(self.pop) + len(self.suep)
+
+
+class MappingEntry:
+    def __init__(self, data: Dict[str, List[str]] | None = None) -> None:
+        self._mappings: Dict[str, List[str]] = data if data is not None else {}
+
+    def __getitem__(self, group_name: str) -> List[str] | None:
+        return self._mappings.get(group_name)
+
+    def __setitem__(self, group_name: str, value: List[str]) -> None:
+        self._mappings[group_name] = value
+
+    def has(self, group_name: str, identifier: str) -> bool:
+        return identifier in group if (group := self[group_name]) is not None else False
+
+    def add(self, group_name: str, identifier: str) -> None:
+        if (group := self[group_name]) is None:
+            self[group_name] = [identifier]
+        else:
+            group.append(identifier)
+
+    def dict(self) -> Dict[str, List[str]]:
+        return self._mappings
+
+
+class MappingList(ReadableJson, WritableJson):
+    def __init__(self, data: Dict[str, Dict[str, List[str]]] | None = None) -> None:
+        self._mappings: Dict[str, MappingEntry] = (
+            {key: MappingEntry(data=entry) for key, entry in data.items()}
+            if data is not None
+            else {}
+        )
+
+    def get_group(self, id: str) -> MappingEntry | None:
+        return self._mappings.get(id)
+
+    def set_group(self, id: str, value: MappingEntry) -> None:
+        self._mappings[id] = value
+
+    def mapping_for_identifier(self, group: str, identifier: str) -> MappingEntry | None:
+        for mapping in self._mappings.values():
+            if mapping.has(group, identifier):
+                return mapping
+        return None
+
+    def add_mapping(
+        self, first_group: str, first_identifier: str, second_group: str, second_identifier: str
+    ) -> None:
+        if (mapping := self.mapping_for_identifier(first_group, first_identifier)) is not None:
+            mapping.add(second_group, second_identifier)
+        else:
+            self.set_group(
+                uuid4().hex,
+                MappingEntry(
+                    data={first_group: [first_identifier], second_group: [second_identifier]}
+                ),
+            )
+
+    def dict(self) -> Dict[str, Dict[str, List[str]]]:
+        return {key: mapping.dict() for key, mapping in self._mappings.items()}
+
+    def to_json(self, indent: int | None = None, *args, **kwargs):
+        return json.dumps(self.dict(), indent=indent)
