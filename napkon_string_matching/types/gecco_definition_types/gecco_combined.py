65ff36843ab1054f161a98db8c69e6606bbc4389
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-12-12 11:11:19 +0000

    GeccoDefinition: no manual switch between JSON and Excel files

--- napkon_string_matching/types/gecco_definition_types/gecco_combined.py
+++ napkon_string_matching/types/gecco_definition_types/gecco_combined.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 
+from napkon_string_matching.types.gecco_definition import GeccoDefinition
 from napkon_string_matching.types.gecco_definition_types.gecco83 import Gecco83Definition
 from napkon_string_matching.types.gecco_definition_types.geccoplus import GeccoPlusDefinition
 
 
 class GeccoCombinedDefinition(Gecco83Definition, GeccoPlusDefinition):
     @staticmethod
-    def read_original_format(gecco83_file: str | Path, geccoplus_file: str | Path, *args, **kwargs):
+    def read_original_format(
+        gecco83_file: str | Path,
+        geccoplus_file: str | Path,
+        file_name: str | Path | None = None,
+        *args,
+        **kwargs
+    ):
+        if file_name is not None and Path(file_name).exists():
+            return GeccoDefinition.read_original_format(file_name)
         gecco = Gecco83Definition.read_original_format(gecco83_file)
         geccoplus = GeccoPlusDefinition.read_original_format(geccoplus_file)
-        return gecco.concat(geccoplus)
+        result: GeccoDefinition = gecco.concat(geccoplus)
+        if file_name is not None:
+            result.write_json(file_name)
+
+        result._extend_parameters()
+        return result
